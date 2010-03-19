470b500b80a85edaa57988cba15a621736aa727c
Rafael <rafaeldx7@gmail.com>
2010-03-19 12:58:42 -0300

    Atualizando na aula de 2010-03-19

--- 04-Jogador/CriaEMostraArquivosDeObjetos.java
+++ 04-Jogador/CriaEMostraArquivosDeObjetos.java
@@ -1,17 +1,24 @@
 import java.io.*;
 
 public class CriaEMostraArquivosDeObjetos {
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     String filename = "/tmp/teste.txt";
     
-    ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename));
+    try {
+      ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename));
     
-    out.writeObject(new Jogador(10, 10, 195, 100, "Teste", "Atacante", 1.99));
-    out.close();
+      out.writeObject(new Jogador(10, 10, 195, 100, "Teste", "Atacante", 1.99));
+      out.close();
+    } catch (Exception e) {
+      System.out.println("Erro na gravação do arquivo");
+    }
     
-    ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
-    
-    Jogador x = (Jogador) in.readObject();
-    System.out.println(x);
+    try {
+      ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
+      Jogador x = (Jogador) in.readObject();
+      System.out.println(x);
+    } catch (Exception e) {
+      System.out.println("Erro na leitura do arquivo");
+    }
   }
 }
