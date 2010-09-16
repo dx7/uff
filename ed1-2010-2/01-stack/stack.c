fc0d7e8351b991c16f87dab820f38a8dcacd8ab1
rafaeldx7 <rafaeldx7@gmail.com>
2010-09-16 14:32:15 -0300

    add trabalho 1

---
+++ ed1-2010-2/01-stack/stack.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#define MAX 50
+
+int main ()
+{
+	char expression[] = "93322$*4/*+";
+	int r = avalia(expression);
+	
+	printf("expressão: %s\nresposta: %i\n", expression, r);
+	
+	return 0;
+}
+
+int avalia(char pos[])
+{
+	int top = -1, i, op1, op2, result, current;
+	char stack[MAX];
+	
+	for(i = 0; i < strlen(pos); ++i)
+	{
+		if(pos[i] >= '0' && pos[i] <= '9')
+		{
+			current = pos[i] - '0';
+			push(&stack, &top, current);
+		}
+		else
+		{
+			op2 = pop(&stack, &top);
+			op1 = pop(&stack, &top);
+
+			if(pos[i] == '+') result = op1 + op2;
+			else if(pos[i] == '-') result = op1 - op2;
+			else if(pos[i] == '*') result = op1 * op2;
+			else if(pos[i] == '/') result = op1 / op2;
+			else if(pos[i] == '$') result = pow(op1, op2);
+
+			push(&stack, &top, result);
+		}
+	}
+	
+	result = pop(&stack, &top);
+	
+	return result;
+}
+
+int pop(char *stack, int *top)
+{
+	if (*top < 0) return 0;
+	
+	int element = *(stack + *top);
+	*top = *top - 1;
+	
+	return element;
+}
+
+int push (char *stack, int *top, int value)
+{
+	if (*top == MAX-1) return 0;
+	
+	*top = *top + 1;
+	*(stack + *top) = value;
+
+	return 1;
+}
+
+show (char *stack, int top)
+{
+	int i;
+	
+	printf("Listando a pilha...\n");
+	
+	for(i = 0; i <= top; ++i) printf("Posição %d ~> %c\n", i, *(stack + i));
+	
+	printf("Fim da listagem.\n\n");
+}
